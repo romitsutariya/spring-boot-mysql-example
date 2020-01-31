3bae112f5f8ad231ad679efc4dd35ad27b4fa219
romitsutariya <romitsutariya@gmail.com>
2020-02-01 00:26:30 +0530

    change in file

--- src/main/java/guru/springframework/SpringBootMySqlApplication.java
+++ src/main/java/guru/springframework/SpringBootMySqlApplication.java
@@ -2,8 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringBootMySqlApplication {
 
 	public static void main(String[] args) {
